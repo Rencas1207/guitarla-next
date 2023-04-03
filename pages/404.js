20395212c8e80d09ae524be6a3393d683b09e40e
Rencas <renzo.c@ohecommerce.agency>
2023-04-03 02:53:16 -0500

    guitarla-next show message error - 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Layout from '@/components/layout';
+import Link from 'next/link';
+
+export default function Page404() {
+  return (
+    <Layout title="Página no encontrada">
+      <p className="error">Página no encontrada</p>
+      <Link href="/" className="error-enlace">
+        Ir a inicio
+      </Link>
+    </Layout>
+  );
+}
